9f0dfaeec3a89ef867a2fd3dda6b7312e3aed138
bIT-Craft-App <88532436+bIT-Craft-App@users.noreply.github.com>
2021-08-28 17:57:53 +0900

    Create LogInScreen & SignUpScreen

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import SignUpScreen from './src/screens/SignUpScreen';
+// import LogInScreen from './src/screens/LogInScreen';
 // import MemoCreateScreen from './src/screens/MemoCreateScreen';
-import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
   return (
-    <MemoEditScreen />
+    <SignUpScreen />
   );
 }
