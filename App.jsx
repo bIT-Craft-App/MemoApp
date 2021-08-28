d3cfca60295a2e718c7ec21a63f0b5423373c00e
bIT-Craft-App <88532436+bIT-Craft-App@users.noreply.github.com>
2021-08-28 16:49:54 +0900

    Creat Create MemoEditScreen & MemoCreateScreen

--- App.jsx
+++ App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import MemoListScreen from './src/screens/MemoListScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
   return (
-    <MemoListScreen />
+    <MemoCreateScreen />
   );
 }
