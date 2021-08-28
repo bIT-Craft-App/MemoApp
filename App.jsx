9b38a8570d069e85fc632004c82e9c2495524b57
bIT-Craft-App <88532436+bIT-Craft-App@users.noreply.github.com>
2021-08-28 16:59:26 +0900

    Replace KeyboadAvoidingView with KeyboardSafeView

--- App.jsx
+++ App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import MemoCreateScreen from './src/screens/MemoCreateScreen';
-// import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoCreateScreen from './src/screens/MemoCreateScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
   return (
-    <MemoCreateScreen />
+    <MemoEditScreen />
   );
 }
