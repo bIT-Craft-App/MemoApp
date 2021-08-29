f9241f75bacb072e397937f0c8ef119c7f3e69a9
bIT-Craft-App <88532436+bIT-Craft-App@users.noreply.github.com>
2021-08-30 02:02:37 +0900

    Implement signUp & LogIn

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
-
+import firebase from 'firebase';
 import SignUpScreen from './src/screens/SignUpScreen';
 import LogInScreen from './src/screens/LogInScreen';
 import MemoCreateScreen from './src/screens/MemoCreateScreen';
@@ -11,11 +11,24 @@ import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 const Stack = createStackNavigator();
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyACe6T8FdJZzDcFfL8pBl7Z31VS_aBVT_I',
+  authDomain: 'memoapp-59b6e.firebaseapp.com',
+  projectId: 'memoapp-59b6e',
+  storageBucket: 'memoapp-59b6e.appspot.com',
+  messagingSenderId: '115951935335',
+  appId: '1:115951935335:web:9d255c58e2690a2088d9c2',
+};
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="LogIn"
+        initialRouteName="SignUp"
         screenOptions={{
           headerStyle: { backgroundColor: '#467FD3' },
           headerTitleStyle: { color: '#ffffff' },
